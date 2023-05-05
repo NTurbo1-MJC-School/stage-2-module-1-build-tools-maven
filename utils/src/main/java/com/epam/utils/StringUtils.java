98613c30d6ad9b6bd02b0e8887847d8a652480b4
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2023-05-06 03:53:37 +0600

    Fifth solution

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,10 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
 
+        if (str == null) {
+            return false;
+        }
+
         float x = 0;
 
         try {
@@ -11,7 +15,7 @@ public class StringUtils {
             return false;
         }
 
-        if (x > 0) {
+        if (x >= 0) {
             return true;
         }
 
